63891c1f85da4d80d677dd6af896ac5d1fdadb4c
definingmoments <definingmoments.io@gmail.com>
2019-08-20 11:20:02 -0400

    added new image bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -77,13 +77,13 @@
 			<section id="colorlib-hero" class="js-fullheight" data-section="home">
 				<div class="flexslider js-fullheight">
 					<ul class="slides">
-				   	<li style="background-image: url(images/img_bg_3.jpg);">
+				   	<li style="background-image: url(images/img-13.jpg);">
 				   		<div class="overlay"></div>
 				   		<div class="container-fluid">
 				   			<div class="row">
 					   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 					   				<div class="slider-text-inner js-fullheight">
-					   					<div class="desc">
+					   					<div class="desc me">
 						   					<h1>Hey <br>my name is David</h1>
 						   					<h2>Welcome to Defining Moment Technologies</h2>
 												<!-- <p><a class="btn btn-primary btn-learn">Download CV <i class="icon-download4"></i></a></p> -->
@@ -93,14 +93,14 @@
 					   		</div>
 				   		</div>
 				   	</li>
-				   	<li style="background-image: url(images/img_bg_prime.jpg);">
+				   	<li style="background-image: url(images/img-7.jpg);">
 				   		<div class="overlay"></div>
 				   		<div class="container-fluid">
 				   			<div class="row">
 					   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 					   				<div class="slider-text-inner">
 					   					<div class="desc">
-						   					<h1>I'm great @ </h1> <h2><br>Cloud Computing<br>Web & App development<br>DevSecOps Consultant</h2>
+						   					<h1>I'm great @ </h1> <h2><br>Cloud Computing<br>Web & App development<br>DevOps Consulting</h2>
 												<p><a class="btn btn-primary btn-learn">View Portfolio <i class="icon-briefcase3"></i></a></p>
 											</div>
 					   				</div>
